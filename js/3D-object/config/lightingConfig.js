e3c5e7849daafcc4fd226c9bbb1e0cfa64e40f13
AndrewStartcev <andrewstarcevmarty@gmail.com>
2023-09-23 15:57:43 +0800

    add nav menu

--- js/3D-object/config/lightingConfig.js
+++ js/3D-object/config/lightingConfig.js
@@ -1,8 +1,8 @@
 // lightingConfig.js
 export const lightingConfig = {
   ambientLightColor: 0xFFFFFF,
-  ambientLightIntensity: 0.8,
+  ambientLightIntensity: 0.9,
   directionalLightColor: 0xFFE8A3,
-  directionalLightIntensity: 2,
+  directionalLightIntensity: 5,
   directionalLightPosition: { x: 7, y: 1, z: 15 },
 };
